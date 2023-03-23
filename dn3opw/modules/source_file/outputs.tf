374364b882290abb2848d72b90afaeb9867fb633
Katona Bálint Sándor <balintgamer14@gmail.com>
2023-03-23 13:00:49 +0100

    2nd_lesson upload

---
+++ dn3opw/modules/source_file/outputs.tf
@@ -0,0 +1,11 @@
+output "file_name"{
+    value=local_file.foo.filename
+}
+
+output "file_permission"{
+    value=local_file.foo.file_permission
+}
+
+output "file_content_base64"{
+    value=local_file.foo.content_base64sha256
+}
