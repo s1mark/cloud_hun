861eb07ce16c67fe56e2ab0eb7d9b851ec8dd598
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2023-03-30 12:19:23 +0200

    Update main.tf

--- C901EE/modules/service-account/main.tf
+++ C901EE/modules/service-account/main.tf
@@ -1,6 +1,6 @@
 module "service_account" {
   source        = "terraform-google-modules/service-accounts/google"
-  project_id    = "cloud1"
+  project_id    = "ekke-cloud"
   names         = ["C901EE"]
   project_roles = ["${var.project_id}=>roles/viewer"]
   display_name  = "Single Account"
