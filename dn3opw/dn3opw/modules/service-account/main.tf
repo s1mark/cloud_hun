94a8fa0e3e77ec43895f4d61104a1e180199eba0
Katona Bálint Sándor <balintgamer14@gmail.com>
2023-03-30 12:19:56 +0200

    single account upload

--- dn3opw/dn3opw/modules/service-account/main.tf
+++ dn3opw/dn3opw/modules/service-account/main.tf
@@ -1,6 +1,6 @@
 module "service_accounts" {
   source        = "terraform-google-modules/service-accounts/google"
-  project_id    = "ekke_cloud"
+  project_id    = "ekke-cloud"
   names         = ["DN3OPW"]
   project_roles = ["roles/viewer"]
   display_name  = "Single Account"
