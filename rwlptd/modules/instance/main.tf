dbb644625e35a9db1b731ec9b0e629fd2770c0c5
unknown <catsharkshin@gmail.com>
2023-03-30 12:45:01 +0200

    instance_template and an example instance

---
+++ rwlptd/modules/instance/main.tf
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+provider "google" {
+
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_compute_address" "ip_address" {
+  name = "external-ip"
+}
+
+locals {
+  access_config = {
+    nat_ip       = google_compute_address.ip_address.address
+    network_tier = "PREMIUM"
+  }
+}
+
+module "instance_template" {
+  source                       = "../instance_template"
+  project_id                   = var.project_id
+  subnetwork                   = var.subnetwork
+  stack_type                   = "IPV4_ONLY"
+  service_account              = var.service_account
+  name_prefix                  = "simple"
+  tags                         = var.tags
+  labels                       = var.labels
+  access_config                = [local.access_config]
+  enable_nested_virtualization = var.enable_nested_virtualization
+  threads_per_core             = var.threads_per_core
+  machine_type = "f1_micro"
+}
